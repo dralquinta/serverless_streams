4c27e71f290a4d304df408acdc4b39bb05a3feaf
dralquinta <dralquinta@gmail.com>
2022-03-30 16:41:05 +0000

    Functional testing against a stream

--- streams_manipulation_poc/func.py
+++ streams_manipulation_poc/func.py
@@ -2,8 +2,11 @@ import io
 import json
 from fdk import response 
 from kafka import KafkaConsumer
+from confluent_kafka import Consumer
+import certifi
 import traceback
 import logging
+import time
 
 # Based on the following documentatioN: 
 # Pre-requisites: https://docs.oracle.com/en/cloud/paas/integration-cloud/stream-service-adapter/prerequisites-creating-connection.html
@@ -11,18 +14,17 @@ import logging
 # Code: https://blogs.oracle.com/developers/post/integrating-oracle-functions-and-oracle-streaming-service
 
 __REGION='sa-santiago-1'
-__STREAM_NAME='My_Stream'
+__STREAM_NAME='sample_stream'
 __TENANCY_NAME='ecrcloud'
-__STREAMPOOL_OCID='ocid1.streampool.oc1.sa-santiago-1.amaaaaaatwfhi7ya66xboit4wmfpyibkv2ekosw2foxxlsvhcqijnemmtooa'
-__OCI_USERNAME='ecrcloud/oracleidentitycloudservice/denny.alquinta@oracle.com/'
+__STREAMPOOL_OCID='ocid1.streampool.oc1.sa-santiago-1.amaaaaaatwfhi7yann7kjkdd4ftxo6xjzutflmqvb7za3xovrm4nc6qwmxia'
+__OCI_USERNAME='oracleidentitycloudservice/denny.alquinta@oracle.com'
 __SASL_USERNAME=__TENANCY_NAME+'/'+__OCI_USERNAME+'/'+__STREAMPOOL_OCID
 __SASL_TOKEN='T_YUSe3W7VIbMui2kbj:'
 
 
 
-def handler(ctx, data: io.BytesIO = None):   
-  #  resp = { "All clear here!": "OK"}
-    resp = consumer(__SASL_USERNAME, __SASL_TOKEN, __REGION, __STREAM_NAME)
+def handler(ctx, data: io.BytesIO = None):       
+    resp = oci_consumer(__STREAM_NAME, __SASL_USERNAME, __SASL_TOKEN, __REGION)
     return response.Response(
             ctx,
             response_data=json.dumps(resp),
@@ -30,23 +32,53 @@ def handler(ctx, data: io.BytesIO = None):
     )      
 
 
-def consumer(sasl_username, sasl_token, region, stream_name):
-    try: 
-        consumer = KafkaConsumer(
-                            stream_name, 
-                            bootstrap_servers = 'cqijnemmtooa.streaming.'+region+'.oci.oraclecloud.com:9092', 
-                            security_protocol = 'SASL_SSL', sasl_mechanism = 'PLAIN',
-                            consumer_timeout_ms = 10000, auto_offset_reset = 'earliest',
-                            group_id='group-0',
-                            sasl_plain_username = sasl_username, 
-                            sasl_plain_password = sasl_token)       
-        returnable_value = "Topic: "+consumer.topic+" Partition: "+consumer.partition+" Offset: "+ consumer.offset +" Key: "+consumer.key +" Value: "+consumer.value   
-        response = { returnable_value: "OK"}
-    except Exception as e:               
+def oci_consumer(stream_name, sasl_username, sasl_token, region):
+    before=time.time()
+    topic = stream_name
+    conf = {  
+        'bootstrap.servers': 'cell-1.streaming.'+region+'.oci.oraclecloud.com', 
+        'security.protocol': 'SASL_SSL',       
+        'ssl.ca.location': certifi.where(),   
+        'sasl.mechanism': 'PLAIN',
+        'sasl.username': sasl_username,  
+        'sasl.password': sasl_token,  
+        'group.id':'group-0',
+        'api.version.request': False,
+        'session.timeout.ms': 6000,
+    }  
+
+    # Create Consumer instance
+    consumer = Consumer(conf)
+
+    # Subscribe to topic
+    consumer.subscribe([topic])
+
+    # Process messages
+    try:        
+        msg = consumer.poll(0.5)
+        if msg is None:
+            # No message available within timeout.
+            # Initial message consumption may take up to
+            # `session.timeout.ms` for the consumer group to
+            # rebalance and start consuming
+            print("Waiting for message or event/error in poll()")       
+            response = { "No new messages": "OK", "Time": time.time()-before }     
+            
+        else:
+            # Check for Kafka message
+            record_key = "Null" if msg.key() is None else msg.key().decode('utf-8')
+            record_value = msg.value().decode('utf-8')         
+            returnable_value = "Consumed record with key "+ record_key + " and value " + record_value
+            response = { returnable_value: "OK", "Time": time.time()-before}
+            consumer.close()
+            
+    except Exception as e:
         response = {"Error: "+str(e): "FAULT"}
         logging.exception(e, exc_info=True)
+        consumer.close()
     return response
-
-
+    
+        
+        
 
 
